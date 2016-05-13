6a30b27761024e99a733e05911080ed33ae00418
lijianzwm <lijianzhanwuming@163.com>
2016-05-13 15:54:17 +0800

    添加阶段性共修数据库表

--- 数据库设计.sql
+++ 数据库设计.sql
@@ -40,3 +40,11 @@ CREATE TABLE `gx_month_ranklist` (
   KEY `index_yearmonth` (`yearmonth`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `gx_stage_gx` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id，唯一自增',
+  `title` varchar(100) NOT NULL COMMENT '共修主题',
+  `num` bigint(30) NOT NULL COMMENT '总数',
+  `beg_date` date NOT NULL COMMENT '开始日期',
+  `end_date` date NOT NULL COMMENT '结束日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
