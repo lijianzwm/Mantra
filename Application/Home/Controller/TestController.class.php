49003dbfc95113529a0f32719a69d10bb1cce654
lijianzwm <lijianzhanwuming@163.com>
2016-05-17 22:25:40 +0800

    debug

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -12,7 +12,7 @@ use Common\Service\UserService;
 
 class TestController{
     public function index(){
-        if( UserService::checkChineseName("李健") ){
+        if( UserService::checkChineseName("李小健") ){
             dump("is Chinese");
         }else{
             dump("not Chinese");
