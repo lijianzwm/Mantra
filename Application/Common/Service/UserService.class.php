06ef5bdf55dd770e9bf51e2b9a6073ba94cba716
lijianzwm <lijianzhanwuming@163.com>
2016-05-18 20:10:12 +0800

    debug

--- Application/Common/Service/UserService.class.php
+++ Application/Common/Service/UserService.class.php
@@ -124,11 +124,10 @@ class UserService{
     }
 
     public static function checkUsernameFormat($username ){
-        $patrn = "/^[\\u4E00-\\u9FA5A-Za-z0-9_]+$/";
         if( strlen($username) > 10 || strlen($username)< 2 ){
             return false;
         }
-        if( preg_match($patrn, $username) ){
+        if( preg_match("/^[\xE00-\x9FA5A-Za-z0-9_]+$/", $username) ){
             return true;
         }else{
             return false;
