b5a202d75179f21c5c23b4f94b346344637977b3
lijianzwm <lijianzhanwuming@163.com>
2016-05-20 12:25:22 +0800

    [lindex $argv 0]

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+git commit -am '[lindex $argv 0]'
+git push
+#!/usr/bin/expect
+# 自动发布到服务器
+spawn  ssh root@139.129.22.123
+send "~/update.sh\r"
+expect "#"
+send "exit\r"
+   expect eof
