53ba2524331238a58ada67c562e46464652bada2
lijianzwm <lijianzhanwuming@163.com>
2016-05-21 13:44:01 +0800

    更新自动发布脚本

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect
 #登录服务器并更新代码
 spawn  ssh root@139.129.22.123
-send "cd /alidata/www/default\r"
+send "./update.sh\r"
 expect "#"
-send "git pull\r"
-expect "#"
+#send "git pull\r"
+#expect "#"
 send "exit\r"
 expect eof
 
