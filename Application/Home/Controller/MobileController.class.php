0c5c35ec852e37e76a55f6a064f74eb884c5cee1
利剑斩无明 <lijianzhanwuming@163.com>
2016-07-24 14:58:00 +0800

    完善报数页面和排行页面

--- Application/Home/Controller/MobileController.class.php
+++ Application/Home/Controller/MobileController.class.php
@@ -7,11 +7,21 @@
  */
 
 namespace Home\Controller;
-
+use Common\Service\CountinService;
+use Common\Service\BrowserService;
+use Common\Service\UserService;
 
 class MobileController extends CommonController{
     public function index(){
         layout(false);
+        $userid = session("userid");
+        $todayNum = CountinService::getUserTodayNumById($userid);
+        $user = UserService::getUserById($userid);
+
+        $this->assign("userid", $userid);
+        $this->assign("showname", $user['showname']);
+        $this->assign("todayNum", $todayNum);
+
         $this->display();
     }
 }
