d32da5ba2d52a5bc712a657bf0c1b7c36a82afba
利剑斩无明 <lijianzhanwuming@163.com>
2016-09-04 18:58:26 +0800

    添加直播相关代码

---
+++ Application/Home/Controller/LiveController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 16/9/4
+ * Time: 下午6:55
+ */
+
+namespace Home\Controller;
+
+
+use Think\Controller;
+
+class LiveController extends Controller{
+    public function live(){
+        $this->display();
+    }
+}
