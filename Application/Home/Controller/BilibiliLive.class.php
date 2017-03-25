78ec610ab40e0c1b70871000d4c8484224ab6f4f
lijian <lijian8931@163.com>
2017-03-25 13:17:34 +0800

    add flv.js

---
+++ Application/Home/Controller/BilibiliLive.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 17/3/24
+ * Time: 11:26
+ */
+
+namespace Home\Controller;
+
+
+use Think\Controller;
+
+class BiliiliLiveController extends Controller{
+    public function index(){
+        $this->display();
+    }
+}
