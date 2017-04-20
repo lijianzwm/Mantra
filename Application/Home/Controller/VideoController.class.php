8278bf32a1f16cabe81f41ae870c918f87eb342e
lijian <lijian8931@163.com>
2017-04-20 23:38:46 +0800

    添加视频播放功能

---
+++ Application/Home/Controller/VideoController.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 17/4/20
+ * Time: 23:23
+ */
+namespace Home\Controller;
+
+use Think\Controller;
+
+class LoginController extends Controller{
+
+    public function player(){
+        $videoid = I("video");
+        switch ($videoid) {
+            case '170415': $videoid = "d4806164b73f48388ecff6d6c31960cb"; break;
+            default: $videoid = "0";
+        }
+        $this->assign("vid", $videoid);
+        $this->display();
+    }
+
+}
